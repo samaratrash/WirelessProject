dcf8cbb5036d64b3722c047d824250bb80f6c137
samaratrash <samaratrash25@gmail.com>
2025-06-28 16:02:11 +0300

    Update main.py

--- wireless_project/main.py
+++ wireless_project/main.py
@@ -504,5 +504,6 @@ def q4():
 
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 10000))  # Default to 10000 if PORT not set
+    app.run(host="0.0.0.0", port=port, debug=True)
 
